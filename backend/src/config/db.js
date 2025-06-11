8e11aacfd7c6a048e0d330b79ef91683432512b2
AlexStrydis <alex.strydis@gmail.com>
2025-06-11 12:36:02 +0300

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   backend/src/config/db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'your_password',
+  password: 'Zagori69!',
   database: 'workshop_management',
   waitForConnections: true,
   connectionLimit: 10,
